c42bc8edf92c3a70c66ee573411069748569aaa8
Adrish <runakarak@gmail.com>
2026-02-22 22:36:01 +0530

    yy

---
+++ app/schemas/auth.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+    name: z.string().min(3).max(30),
+    email: z.email(),
+    password: z.string().min(8).max(16),
+});
+
+export type SignUpSchemaType = z.infer<typeof signupSchema>;
