d7328142e8507cab4d86c8106c4997b23e03a366
Adrish <runakarak@gmail.com>
2026-02-28 19:42:58 +0530

    eed

---
+++ convex/posts.ts
@@ -0,0 +1,20 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { authComponent } from "./auth";
+
+//Create a new post
+export const createPost = mutation({
+    args: { title: v.string(), body: v.string() },
+    handler: async (ctx, args) => {
+        const user = await authComponent.safeGetAuthUser(ctx);
+        if (!user) {
+            throw new ConvexError("Unauthorized");
+        }
+        const blogArticle = await ctx.db.insert("posts", {
+            title: args.title,
+            body: args.body,
+            authorId: user._id
+        })
+        return blogArticle;
+    }
+})
