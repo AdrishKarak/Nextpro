63b491d61cd5dc6e27ea102c628c8bd16b81c1a8
Adrish <runakarak@gmail.com>
2026-03-10 22:11:04 +0530

    feat: Implement skeleton loading UI for the blog page by creating a new Skeleton component and refactoring data fetching with Suspense.

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,15 +1,16 @@
-"use client"
+
 
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api"
-import { useQuery } from "convex/react"
+import { fetchQuery } from "convex/nextjs";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function BlogPage() {
-    const data = useQuery(api.posts.getPosts);
     return (
         <div className="py-12">
             <div className="text-center pb-12">
@@ -18,26 +19,56 @@ export default function BlogPage() {
                     Read the latest blogs and stories from our community
                 </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-                {data?.map((post) => (
-                    <Card key={post._id} className="pt-0">
-                        <div className="relative h-48 w-full overflow-hidden">
-                            <Image src="" alt="" width={500} height={500} fill className="rounded-t-lg" />
-                        </div>
 
-                        <CardContent>
-                            <Link href={`/blog/${post._id}`}>
-                                <h1 className="text-2xl font-bold hover:text-primary">{post.title}</h1>
-                            </Link>
-                            <p className="text-muted-foreground line-clamp-3">{post.body}</p>
-                        </CardContent>
-                        <CardFooter>
-                            <Link className={buttonVariants({ className: "w-full" })} href={`/blog/${post._id}`}>Read More <span><ArrowRight /></span></Link>
-                        </CardFooter>
-                    </Card>
-                ))}
-            </div>
-
+            <Suspense fallback={<SkeletonLoadingUi />}>
+                <LoadBlogList />
+            </Suspense>
         </div>
     )
 };
+
+async function LoadBlogList() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const data = await fetchQuery(api.posts.getPosts);
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+            {data?.map((post) => (
+                <Card key={post._id} className="pt-0">
+                    <div className="relative h-48 w-full overflow-hidden">
+                        <Image src="https://images.unsplash.com/photo-1533150783171-ce47d5c2b6ef?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d2FsbHBhcGVyJTIwaGR8ZW58MHx8MHx8fDA%3D" alt="image" width={500} height={500} className="rounded-t-lg object-cover" />
+                    </div>
+
+                    <CardContent>
+                        <Link href={`/blog/${post._id}`}>
+                            <h1 className="text-2xl font-bold hover:text-primary">{post.title}</h1>
+                        </Link>
+                        <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+                    </CardContent>
+                    <CardFooter>
+                        <Link className={buttonVariants({ className: "w-full" })} href={`/blog/${post._id}`}>Read More <span><ArrowRight /></span></Link>
+                    </CardFooter>
+                </Card>
+            ))}
+        </div>
+    )
+}
+
+function SkeletonLoadingUi() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+            {[
+                ...Array(6)].map((_, i) => (
+                    <div key={i} className="flex flex-col space-y-3">
+                        <Skeleton className="h-48 w-full rounded-xl" />
+                        <div className="space-y-2 flex flex-col">
+                            <Skeleton className="h-6 w-3/4" />
+                            <Skeleton className="h-4 w-full" />
+                            <Skeleton className="h-4 w-2/3" />
+                        </div>
+                    </div>
+
+                ))
+            }
+        </div>
+    )
+}
